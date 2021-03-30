d9e0ca85c8ad39e96339c8df0081e3aeaa77377e
Piero A. Pretto <pretto417@gmail.com>
2021-03-30 09:33:36 +0300

    play with saga

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ import App from './App';
 import reducer from './store/reducer';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { watchAgeUp } from './sagas/saga';
 
-const store = createStore(reducer);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(watchAgeUp);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
